4e29103607c9f87ede7ebb5e91a0e941efd494ed
VickyBondarenko <quikyv@gmail.com>
2023-10-20 17:13:23 +0200

    Add lazy load

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 // import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
-import { Routes, Route } from "react-router-dom";
+import { lazy } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
-import LogInPage from "./pages/LogInPage";
-import PublicRoute from "./hooks/PublicRoute";
-import PrivateRoute from "./hooks/PrivateRoute";
-import SharedLayout from "./components/SharedLayout/SharedLayout";
-import DashboardPage from "./pages/DashboardPage";
-import AllOrdersPage from "./pages/AllOrdersPage";
-import AllProductsPage from "./pages/AllProductsPage";
-import AllSuppliersPage from "./pages/AllSuppliersPage";
-import CustomersDataPage from "./pages/CustomersDataPage";
+
+const LogInPage = lazy(() => import("./pages/LogInPage"));
+const PublicRoute = lazy(() => import("./hooks/PublicRoute"));
+const PrivateRoute = lazy(() => import("./hooks/PrivateRoute"));
+const SharedLayout = lazy(() =>
+  import("./components/SharedLayout/SharedLayout")
+);
+const DashboardPage = lazy(() => import("./pages/DashboardPage"));
+const AllOrdersPage = lazy(() => import("./pages/AllOrdersPage"));
+const AllProductsPage = lazy(() => import("./pages/AllProductsPage"));
+const AllSuppliersPage = lazy(() => import("./pages/AllSuppliersPage"));
+const CustomersDataPage = lazy(() => import("./pages/CustomersDataPage"));
 
 function App() {
   return (
@@ -34,13 +38,14 @@ function App() {
             </PrivateRoute>
           }
         >
-          {/* <Route index element={<DashboardPage />} /> */}
+          <Route index element={<DashboardPage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/orders" element={<AllOrdersPage />} />
           <Route path="/products" element={<AllProductsPage />} />
           <Route path="/suppliers" element={<AllSuppliersPage />} />
           <Route path="/customers" element={<CustomersDataPage />} />
-          {/* <Route path="*" element={<Page404 />} /> */}
+
+          <Route path="*" element={<Navigate to="/dashboard" />} />
         </Route>
       </Routes>
     </>
